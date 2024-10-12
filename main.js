12babff4edf18a3aebfb6f741a19d171b1adc13b
RadovanCipovic <radovan.cipovic@gmail.com>
2024-10-12 15:28:48 +0200

    zavrsavanje flappy

--- main.js
+++ main.js
@@ -1,63 +1,93 @@
-document.addEventListener('DOMContentLoaded', () => {
-const ptica = document.querySelector('.ptica');
-const igricaDisplej = document.querySelector('.game-container');
-const zemlja = document.querySelector('.zemlja');
- 
- let pticaLijevo = 220;
- let pticaDolje = 100;
- let gravitacija = 2;
- 
- function pocniIgru() {
-  pticaDolje -= gravitacija;
-  ptica.style.bottom = pticaDolje + 'px';
-  ptica.style.left = pticaLijevo + 'px';
- }
- pocniIgru();
- 
- let tajmerID = setInterval(pocniIgru, 20);
- 
-// function kontoler(e) {
-//   if(e.keyCode === 32){
-//     skok();
-//   }
-// }
- 
- function skok() {
-   if(pticaDolje < 500) pticaDolje += 50;
-   ptica.style.bottom = pticaDolje + 'px';
-   console.log(pticaDolje)
- }
- document.addEventListener('keyup', skok);
- 
- 
- function generisiPrepreku() {
-   let preprekaLijevo = 500;
-   let randomVisina = Math.random() * 60;
-   let preprekaDolje = randomVisina;
-   
-   const prepreka = document.createElement('div');
-   prepreka.classList.add('prepreka')
-   igricaDisplej.appendChild(prepreka);
-   prepreka.style.left = preprekaLijevo + 'px';
-   prepreka.style.bottom = preprekaDolje + 'px';
-   
-   function pomjeranjePrepreke() {
-   preprekaLijevo -= 2;
-   prepreka.style.left = preprekaLijevo + 'px';
-   
-   if (preprekaLijevo === -60) {
-     clearInterval();
-     igricaDisplej.removeChild(prepreka)
+document.addEventListener("DOMContentLoaded", () => {
+  const ptica = document.querySelector(".ptica");
+  const igricaDisplej = document.querySelector(".game-container");
+  const zemlja = document.querySelector(".zemlja");
+
+  let pticaLijevo = 220;
+  let pticaDolje = 100;
+  let gravitacija = 2;
+  let jeLiKrajIgre = false;
+  let gap = 430;
+
+  function pocniIgru() {
+    pticaDolje -= gravitacija;
+    ptica.style.bottom = pticaDolje + "px";
+    ptica.style.left = pticaLijevo + "px";
+  }
+  pocniIgru();
+
+  let igraTajmerID = setInterval(pocniIgru, 20);
+
+  // function kontoler(e) {
+  //   if (e.keyCode === 32) {
+  //     skok();
+  //   }
+  // }
+
+  // PTICA SKOK
+  function skok() {
+    if (pticaDolje < 500) pticaDolje += 50;
+    ptica.style.bottom = pticaDolje + "px";
+    console.log(pticaDolje);
+  }
+
+  document.addEventListener("keyup", skok);
+
+  // POKRET
+  function generisiPrepreku() {
+    let preprekaLijevo = 500;
+    let randomVisina = Math.random() * 60;
+    let preprekaDolje = randomVisina;
+
+    const prepreka = document.createElement("div");
+    const gorePrepreka = document.createElement("div");
+
+    if (!jeLiKrajIgre) {
+      prepreka.classList.add("prepreka");
+      gorePrepreka.classList.add("gorePrepreka");
     }
-   }
-   let timerId = setInterval(pomjeranjePrepreke, 20);
-   setTimeout(generisiPrepreku, 3000)
-   
- }
- generisiPrepreku();
- 
- 
- 
- 
- 
-}) 
+
+    igricaDisplej.appendChild(prepreka);
+    igricaDisplej.appendChild(gorePrepreka);
+
+    prepreka.style.left = preprekaLijevo + "px";
+    gorePrepreka.style.left = preprekaLijevo + "px";
+    prepreka.style.bottom = preprekaDolje + "px";
+    gorePrepreka.style.bottom = preprekaDolje + gap + "px";
+
+    // PREPREKE - POMJERANJE
+    function pomjeranjePrepreke() {
+      preprekaLijevo -= 2;
+      prepreka.style.left = preprekaLijevo + "px";
+      gorePrepreka.style.left = preprekaLijevo + "px";
+
+      if (preprekaLijevo === -60) {
+        clearInterval();
+        igricaDisplej.removeChild(prepreka);
+        igricaDisplej.removeChild(gorePrepreka);
+      }
+      if (
+        (preprekaLijevo > 200 &&
+          preprekaLijevo < 280 &&
+          pticaLijevo === 220 &&
+          (pticaDolje < preprekaDolje + 150 ||
+            pticaDolje > preprekaDolje + gap - 200)) ||
+        pticaDolje === 0
+      ) {
+        krajIgre();
+        clearInterval(tajmerID);
+      }
+    }
+    let tajmerID = setInterval(pomjeranjePrepreke, 20);
+    if (!jeLiKrajIgre) setTimeout(generisiPrepreku, 3000);
+  }
+  generisiPrepreku();
+
+  // KRAJ IGRE
+  function krajIgre() {
+    clearInterval(igraTajmerID);
+    // console.log("Kraj igre");
+    jeLiKrajIgre = true;
+    document.removeEventListener("keyup", skok);
+  }
+});
